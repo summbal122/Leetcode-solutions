54c61bab2cf0ef37d0fd9b395516439c8b17e70d
Sumbal Ijaz <sunbalijaz13@gmail.com>
2025-07-31 12:02:34 +0500

    Time: 5 ms (62.04%), Space: 55.2 MB (95.62%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    let arr = s.split('');
+    let temp;
+
+    for (let i = 0; i < arr.length; i += 2 * k) {
+        let left = i;
+        let right = Math.min(i + k - 1, arr.length - 1);
+        
+        while (left < right) {
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    return arr.join('');
+};
