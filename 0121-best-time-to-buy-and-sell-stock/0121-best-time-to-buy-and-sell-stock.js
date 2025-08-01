e1b2194a7936b4fbc1ebf8e4d4db6b5b2f9b90c1
Sumbal Ijaz <sunbalijaz13@gmail.com>
2025-08-01 10:37:15 +0500

    Time: 2 ms (83.04%), Space: 65.2 MB (50.95%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = Infinity
+    let profit = 0;
+    for (let i = 0; i<prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        } else if (prices[i] - min > profit){
+            profit = prices[i] - min
+        }
+    } 
+    return profit;
+};
