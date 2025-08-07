6ecdd9f7b6010d747768cd35ad432ecdc5374d4d
Sumbal Ijaz <sunbalijaz13@gmail.com>
2025-08-07 10:21:38 +0500

    Time: 1 ms (63.95%), Space: 56.3 MB (44.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-  const set = new Set(nums);
-  for (let i = 0; i <= nums.length; i++) {
-    if (!set.has(i)) {
-      return i;
-    }
-  }
+let n = nums.length
+ const sum = n * (n + 1) / 2
+ let sumOfNums =  0
+ for (let i = 0; i< n; i++){
+  sumOfNums = sumOfNums + nums[i]
+ }
+ return sum - sumOfNums;
 };
