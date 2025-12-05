752436ebf47e4a020cb92e5b625fde897d79f9cc
Sumbal Ijaz <sunbalijaz13@gmail.com>
2025-12-05 11:29:30 +0500

    Time: 52 ms (81.4%), Space: 59.1 MB (72.85%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,17 +11,19 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-      let slow = head;
+   let slow = head;
   let fast = head;
-  while ( fast !== null && fast.next !== null){
+
+  while (fast !== null && fast.next !== null) {
     slow = slow.next;
-    fast = fast.next.next
-    if (fast === slow){
+    fast = fast.next.next;
+
+    if (slow === fast) {
       return true;
     }
-    
+  }
 
-  } return false;
+  return false;
  
     
 };
