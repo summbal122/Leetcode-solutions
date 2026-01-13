a2d1e396dd5dcd372f0e2ddaee466bc9781f087e
Sumbal Ijaz <sunbalijaz13@gmail.com>
2026-01-13 11:27:23 +0500

    Time: 0 ms (100%), Space: 54.1 MB (90.85%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,22 +11,19 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-     let sentinel = new ListNode();
-   sentinel.next = head;
+  let sentinel = new ListNode();
+  sentinel.next = head;
+  let first = sentinel
+  let second = sentinel
+  for (let i = 0; i< n; i++){
+    first = first.next
+  }
+  while ( first.next){
+    second = second.next
+    first = first.next
+  }
+  second.next = second.next.next
+  return sentinel.next
 
-   let length = 0;
-   while(head){
-    head = head.next
-    length ++
-   }
-   let prevPos = length - n
-   let prev = sentinel;
-
-   for ( let i = 0; i < prevPos ; i++){
-    prev = prev.next
-   }
-   prev.next = prev.next.next
-
-   return sentinel.next
-
+  
 }
