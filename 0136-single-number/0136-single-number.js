7a24ea503aef4c4b4c4ce9444b88e88b04f34357
Sumbal Ijaz <sunbalijaz13@gmail.com>
2026-04-13 01:20:23 +0500

    Time: 4 ms (45.04%), Space: 58.8 MB (23.31%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let hash = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (!hash[nums[i]]) {
-            hash[nums[i]] = 1;
-        } else {
-            hash[nums[i]]++;
-        }
+  let hash = {}
+
+  for(let num of nums){
+    if(hash[num]){
+      hash[num] = hash[num] + 1
+    } else{
+      hash[num] = 1
     }
-    for (let i = 0; i < nums.length; i++) {
-        if (hash[nums[i]] === 1) {
-            return nums[i];
-        }
+  }
+  for(let num of nums){
+    if(hash[num] === 1) {
+      return num
     }
-};
+  }
+  
+}
